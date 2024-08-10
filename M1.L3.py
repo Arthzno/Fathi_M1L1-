efa148ed93f2af6cf3030e0f8ba821a6ce78d1ac
Arthzno <fthayyash@gmail.com>
2024-08-10 17:32:40 +0700

    Update M1.L3.py

--- M1.L3.py
+++ M1.L3.py
@@ -1,7 +1,11 @@
 
 import discord
-import random
 from discord.ext import commands
+import random
+import os
+import requests
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -33,4 +37,53 @@ async def roll (ctx, num_sides: int = 6):
     roll_result = random.randint(1, num_sides)
     await ctx.send(f'You rolled a {roll_result} on a {num_sides}-sided dice!')
 
-bot.run("MTI2NjY5ODQ4MzcwOTExNjQ1Nw.GB-NZq.XbhRpilMpmglAUHR7zuSdr2ZsSch4FtEaIL0F8")
+#MEMES
+@bot.command()
+async def stonk(ctx):
+    with open('images/memes/stonks.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def notstonk(ctx):
+    with open('images/memes/not stonks.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+#STICKER
+@bot.command()
+async def chill(ctx):
+    with open('images/sticker/Chill.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def die(ctx):
+    with open('images/sticker/Die.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+
+#API
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("token ARTH BOT")
